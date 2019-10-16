43aa83e61e1bfc30ade076ac543b4a16156ad9e0
Jithendra V Kumar <33452200+IamJithendra@users.noreply.github.com>
2019-10-16 17:01:21 +0530

    Update translation.py
    change

--- translation.py
+++ translation.py
@@ -7,7 +7,7 @@ Subscribe @SpEcHlDe if you 💔 using this bot."""
 
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "This is an @RenamingRobot clone created using the Open Source <a href='https://github.com/SpEcHiDe/AnyDLBot'>code</a>. \nStart @AnyDLBot to know Paid Plans!"
+    UPGRADE_TEXT = "You Can Pay me in PayPal or Paytm!"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""

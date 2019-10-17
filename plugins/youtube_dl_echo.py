032ab12a40bf9848065882a2fc053d4ba5abfbb3
Jithendra V Kumar <33452200+IamJithendra@users.noreply.github.com>
2019-10-17 21:29:01 +0530

    Update youtube_dl_echo.py
    c

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
-
+ 
 # the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
+ 
 import asyncio
 import json
 import math
 import os
 import time
-
+ 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-
+ 
 # the Strings used for this "thing"
 from translation import Translation
-
+ 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
+ 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import humanbytes
 from helper_funcs.help_uploadbot import DownLoadFile
-
-
+ 
+ 
 @pyrogram.Client.on_message(pyrogram.Filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:

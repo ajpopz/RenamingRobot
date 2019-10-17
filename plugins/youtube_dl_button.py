43c853f44f8e319fb9ba013a37fd779a27f05016
Jithendra V Kumar <33452200+IamJithendra@users.noreply.github.com>
2019-10-17 21:29:47 +0530

    Update youtube_dl_button.py
    c

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
-
+ 
 # the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
+ 
 import asyncio
 import json
 import math
@@ -15,19 +15,19 @@ import os
 import shutil
 import time
 from datetime import datetime
-
+ 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-
+ 
 # the Strings used for this "thing"
 from translation import Translation
-
+ 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
+ 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 from hachoir.metadata import extractMetadata
@@ -35,8 +35,8 @@ from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
 from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
-
-
+ 
+ 
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
     # youtube_dl extractors

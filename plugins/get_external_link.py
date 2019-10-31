7fd98cb8e50dc149d9b5e54a331afb2594fbd069
Jithendra V Kumar <33452200+IamJithendra@users.noreply.github.com>
2019-10-31 19:04:39 +0530

    Update get_external_link.py

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -32,14 +32,13 @@ from helper_funcs.display_progress import progress_for_pyrogram
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["getlink"]))
 async def get_link(bot, update):
-   # if update.from_user.id not in Config.AUTH_USERS:
-   #     await bot.delete_messages(
-   #         chat_id=update.chat.id,
-   #         message_ids=update.message_id,
-   #         revoke=True
-   #     )
-   #     return
-    
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
+            chat_id=update.chat.id,
+            message_ids=update.message_id,
+            revoke=True
+        )
+        return
     TRChatBase(update.from_user.id, update.text, "getlink")
     logger.info(update.from_user)
     if update.reply_to_message is not None:
@@ -97,12 +96,10 @@ async def get_link(bot, update):
         else:
             logger.info(t_response)
             t_response_arry = t_response.decode("UTF-8").split("\n")[-1].strip()
-            shorten_api_url = "http://ouo.io/api/{}?s={}".format(Config.OUO_IO_API_KEY, t_response_arry)
-            adfulurl = requests.get(shorten_api_url).text
         await bot.edit_message_text(
             chat_id=update.chat.id,
-            text=Translation.AFTER_GET_DL_LINK.format(adfulurl, max_days),
-            parse_mode=pyrogram.ParseMode.HTML,
+            text=Translation.AFTER_GET_DL_LINK.format(t_response_arry, max_days),
+            parse_mode="html",
             message_id=a.message_id,
             disable_web_page_preview=True
         )
